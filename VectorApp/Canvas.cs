24a7bc115e6f2e0639f71ffb1766b1c2098fae44
Mustafa KURAL <mustafakural@outlook.com>
2024-11-03 00:29:50 +0300

    flicker çözüldü

--- VectorApp/Canvas.cs
+++ VectorApp/Canvas.cs
@@ -10,29 +10,43 @@ namespace VectorApp
     {
         Point pt1 = new Point();//ilk tıklanan yer
         Point pt2 = new Point();//son bırakılan yer
+        bool isDouseDown = false;
 
-        public Canvas() { }
+        public Canvas() {
+            this.SetStyle(
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint |
+                ControlStyles.DoubleBuffer,
+                true);
+        }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             e.Graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, Width-1, Height-1));
             e.Graphics.DrawRectangle(Pens.Gray, new Rectangle(0, 0, Width - 1, Height - 1));
             ////
-
-            e.Graphics.DrawRectangle(Pens.Black, Rectangle.FromLTRB(pt1.X, pt1.Y, pt2.X, pt2.Y));
+            ///
+            if(isDouseDown)
+                e.Graphics.DrawRectangle(Pens.Black, Rectangle.FromLTRB(pt1.X, pt1.Y, pt2.X, pt2.Y));
         }
 
         protected override void OnMouseDown(MouseEventArgs e)
         {
             pt1 = e.Location;
+            isDouseDown = true;
         }
 
         protected override void OnMouseUp(MouseEventArgs e)
         {
             pt2 = e.Location;
-
             Invalidate();
+            isDouseDown = false;
         }
 
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            pt2 = e.Location;
+            Invalidate();
+        }
     }
 }
