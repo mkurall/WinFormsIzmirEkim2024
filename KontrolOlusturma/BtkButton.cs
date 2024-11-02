c52e98300a2fa29de55d5c2023ef350a0699da97
Mustafa KURAL <mustafakural@outlook.com>
2024-11-02 22:35:11 +0300

    BtkButton

---
+++ KontrolOlusturma/BtkButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontrolOlusturma
+{
+    public class BtkButton : Button
+    {
+        public BtkButton()
+        {
+            BackColor = Color.BlueViolet;
+            ForeColor = Color.White;
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            BackColor = Color.Yellow;
+            ForeColor = Color.Black;
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            BackColor = Color.BlueViolet;
+            ForeColor = Color.White;
+        }
+    }
+}
---
+++ KontrolOlusturma/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace KontrolOlusturma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btkButton1 = new BtkButton();
+            btkButton2 = new BtkButton();
+            SuspendLayout();
+            // 
+            // btkButton1
+            // 
+            btkButton1.BackColor = Color.BlueViolet;
+            btkButton1.ForeColor = Color.White;
+            btkButton1.Location = new Point(125, 104);
+            btkButton1.Name = "btkButton1";
+            btkButton1.Size = new Size(157, 49);
+            btkButton1.TabIndex = 0;
+            btkButton1.Text = "btkButton1";
+            btkButton1.UseVisualStyleBackColor = false;
+            // 
+            // btkButton2
+            // 
+            btkButton2.BackColor = Color.BlueViolet;
+            btkButton2.ForeColor = Color.White;
+            btkButton2.Location = new Point(311, 104);
+            btkButton2.Name = "btkButton2";
+            btkButton2.Size = new Size(148, 49);
+            btkButton2.TabIndex = 1;
+            btkButton2.Text = "btkButton2";
+            btkButton2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btkButton2);
+            Controls.Add(btkButton1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private BtkButton btkButton1;
+        private BtkButton btkButton2;
+    }
+}
